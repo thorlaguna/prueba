1514be12c24137b63916d7d5a17b7ed3f6a75254
Victor Laguna <laguna.bavic18@zaragoza.salesianos.edu>
2019-05-17 19:04:15 +0200

    comit horneado
    Signed-off-by: Victor Laguna <laguna.bavic18@zaragoza.salesianos.edu>

--- Oterooooo.java
+++ Oterooooo.java
@@ -3,8 +3,7 @@ package prueba;
 public class Oterooooo {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		System.out.println("El HORRRRRRRRNO!!!!!!!!!!!!!");
 	}
 
 }
